0b939791c5dd4a7dc9fd5f9f0047b919b4e1dae7
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-13 14:28:11 +0100

    Update-CRUD_fetch

---
+++ src/helpers/helpHttp.js
@@ -0,0 +1,33 @@
+export const helpHttp = () => {
+  const customFetch = (endpoint, options) => {
+    const defaultHeader = {
+      accept: "application/json",
+    };
+
+    const controller = new AbortController();
+
+    options.signal = controller.signal;
+    options.method = options.method || "GET";
+    options.headers = options.headers
+      ? { ...defaultHeader, ...options.headers }
+      : defaultHeader;
+
+    options.body = JSON.stringify(options.body) || false;
+    if (!options.body) delete options.body;
+  };
+
+  const get = () => {};
+
+  const post = () => {};
+
+  const put = () => {};
+
+  const del = () => {};
+
+  return {
+    get,
+    post,
+    put,
+    del,
+  };
+};
