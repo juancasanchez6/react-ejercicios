fa8a167751cff8cecc15b51f0c1c530910c0344f
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-13 13:51:14 +0100

    Update-CRUD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import CrudApp from "./componentes/CrudApp";
+import CrudApi from './componentes/CrudApi';
 
 function App() {
   return (
     < >
      <h1>Ejercicios con React</h1>
-     <CrudApp />
      <hr />
+     <CrudApi />
+     <hr />
+     <CrudApp />
     </>
   );
 }
