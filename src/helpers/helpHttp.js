e46aff6cd039b80d9648fbac8db75909ba678f25
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-14 10:23:42 +0100

    Update-Song_Search

--- src/helpers/helpHttp.js
+++ src/helpers/helpHttp.js
@@ -5,8 +5,8 @@ export const helpHttp = () => {
     };
 
     const controller = new AbortController();
-
     options.signal = controller.signal;
+
     options.method = options.method || "GET";
     options.headers = options.headers
       ? { ...defaultHeader, ...options.headers }
@@ -14,15 +14,39 @@ export const helpHttp = () => {
 
     options.body = JSON.stringify(options.body) || false;
     if (!options.body) delete options.body;
+
+    //console.log(options);
+    setTimeout(() => controller.abort(), 3000);
+
+    return fetch(endpoint, options)
+      .then((res) =>
+        res.ok
+          ? res.json()
+          : Promise.reject({
+              err: true,
+              status: res.status || "00",
+              statusText: res.statusText || "Ocurrió un error",
+            })
+      )
+      .catch((err) => err);
   };
 
-  const get = () => {};
+  const get = (url, options = {}) => customFetch(url, options);
 
-  const post = () => {};
+  const post = (url, options = {}) => {
+    options.method = "POST";
+    return customFetch(url, options);
+  };
 
-  const put = () => {};
+  const put = (url, options = {}) => {
+    options.method = "PUT";
+    return customFetch(url, options);
+  };
 
-  const del = () => {};
+  const del = (url, options = {}) => {
+    options.method = "DELETE";
+    return customFetch(url, options);
+  };
 
   return {
     get,
